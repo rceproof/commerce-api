1a993598d92787e2ff410d946caba7f8a5f33fe0
rceproof <rceproof@outlook.kr>
2026-09-04 11:05:17 +0900

    URL 경로별 라우팅 추가

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('안녕! 첫서버 생성');
+  console.log("요청 들어온 경로:", req.url);
+
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("환영합니다! Commerce API 입니다");
+  } else if (req.url === "/products") {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("상품 목록입니다");
+  } else if (req.url === "/orders") {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("주문 목록입니다");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("페이지를 찾을 수 없습니다");
+  }
 });
 
 server.listen(3000, () => {

cfc1fd92c499d06065c9ecea4f9171afa0671ebd
rceproof <rceproof@outlook.kr>
2026-09-24 10:31:05 +0900

    전역 에러 핸들러로 에러 정보 노출 방어

--- server.js
+++ server.js
@@ -258,6 +258,14 @@ app.use((req, res) => {
   res.status(404).json({ error: '요청하신 경로를 찾을 수 없습니다' });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  if (err.type === 'entity.parse.failed') {
+    return res.status(400).json({ error: '잘못된 JSON 형식입니다' });
+  }
+  res.status(500).json({ error: '서버 오류가 발생했습니다' });
+});
+
 app.listen(3000, () => {
   console.log('서버 실행중 http://localhost:3000');
 });
